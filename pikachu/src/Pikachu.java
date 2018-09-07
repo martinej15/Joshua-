e5b6afbc97ceb48cbffae27986cd033cde396596
martinej15 <martinej15@WHS2301S13.woodland.wednet.edu>
2018-09-07 09:12:43 -0700

    Pie comment

--- pikachu/src/Pikachu.java
+++ pikachu/src/Pikachu.java
@@ -3,6 +3,10 @@ public class Pikachu
 {
 	public static void main(String[] args)
 	{
+		/*double pie = Math.PI;
+		String eatingHabbits = "I tend to eat ";
+		System.out.println(eatingHabbits + pie);*/
+		
 		System.out.println("Pikachu welcomes you to the world of Pokémon!");
 		System.out.println("(\\_/)\n(O^.^)\nZ(_(')(')");
 	}
