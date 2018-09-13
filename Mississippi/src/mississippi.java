037d0c775a097206ef3f707e70aa7c61d2b31f85
martinej15 <42974413+martinej15@users.noreply.github.com>
2018-09-13 11:57:40 -0700

    Mississippi

---
+++ Mississippi/src/mississippi.java
@@ -0,0 +1,74 @@
+
+public class mississippi {
+
+	public static void main(String[] args) {
+		System.out.println(m() + m());
+		/*i();
+		s();
+		s();
+		i();
+		s();
+		s();
+		i();
+		p();
+		p();
+		i();*/
+	}
+	
+	public static String m ()
+	{
+		/*System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M \n");*/
+		String m =  "M     M\n" +
+			        "MM   MM\n" +
+			        "M M M M\n" +
+			        "M  M  M\n" +
+			        "M     M\n" +
+			        "M     M\n" +
+			        "M     M\n";
+		return m;
+
+	}
+
+	public static void i ()
+	{
+		System.out.println("IIIII");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("IIIII\n");
+
+	}
+
+	public static void s ()
+	{
+		System.out.println(" SSSSS");
+		System.out.println("S     S");
+		System.out.println("S");
+		System.out.println(" SSSSS");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS\n");
+
+	}
+
+	public static void p ()
+	{
+		System.out.println("PPPPPP");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P\n");
+
+	}
+
+}
